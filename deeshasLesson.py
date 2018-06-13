303fcbb5dbc06a12d80ea8ffdfbc40b69b49eae6
Lord Afzal <afzal.eliyah@gmail.com>
2018-06-12 22:28:25 -0400

    slay

--- deeshasLesson.py
+++ deeshasLesson.py
@@ -28,3 +28,63 @@ eth_market_info = pd.read_html("https://coinmarketcap.com/currencies/ethereum/hi
 eth_market_info = eth_market_info.assign(Date=pd.to_datetime(eth_market_info['Date']))
 # look at the first few rows
 eth_market_info.head()
+# getting the Bitcoin and Eth logos
+import sys
+from PIL import Image
+import io
+
+if sys.version_info[0] < 3:
+    import urllib2 as urllib
+    bt_img = urllib.urlopen("http://logok.org/wp-content/uploads/2016/10/Bitcoin-Logo-640x480.png")
+    eth_img = urllib.urlopen("https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png")
+else:
+    import urllib
+    bt_img = urllib.request.urlopen("http://logok.org/wp-content/uploads/2016/10/Bitcoin-Logo-640x480.png")
+    eth_img = urllib.request.urlopen("https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png")
+
+image_file = io.BytesIO(bt_img.read())
+bitcoin_im = Image.open(image_file)
+
+image_file = io.BytesIO(eth_img.read())
+eth_im = Image.open(image_file)
+width_eth_im , height_eth_im  = eth_im.size
+eth_im = eth_im.resize((int(eth_im.size[0]*0.8), int(eth_im.size[1]*0.8)), Image.ANTIALIAS)
+bitcoin_market_info.columns =[bitcoin_market_info.columns[0]]+['bt_'+i for i in bitcoin_market_info.columns[1:]]
+eth_market_info.columns =[eth_market_info.columns[0]]+['eth_'+i for i in eth_market_info.columns[1:]]
+fig, (ax1, ax2) = plt.subplots(2,1, gridspec_kw = {'height_ratios':[3, 1]})
+ax1.set_ylabel('Closing Price ($)',fontsize=12)
+ax2.set_ylabel('Volume ($ bn)',fontsize=12)
+ax2.set_yticks([int('%d000000000'%i) for i in range(10)])
+ax2.set_yticklabels(range(10))
+ax1.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax1.set_xticklabels('')
+ax2.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax2.set_xticklabels([datetime.date(i,j,1).strftime('%b %Y')  for i in range(2013,2019) for j in [1,7]])
+ax1.plot(bitcoin_market_info['Date'].astype(datetime.datetime),bitcoin_market_info['bt_Open'])
+ax2.bar(bitcoin_market_info['Date'].astype(datetime.datetime).values, bitcoin_market_info['bt_Volume'].values)
+fig.tight_layout()
+fig.figimage(bitcoin_im, 100, 120, zorder=3,alpha=.5)
+plt.show()
+fig, (ax1, ax2) = plt.subplots(2,1, gridspec_kw = {'height_ratios':[3, 1]})
+#ax1.set_yscale('log')
+ax1.set_ylabel('Closing Price ($)',fontsize=12)
+ax2.set_ylabel('Volume ($ bn)',fontsize=12)
+ax2.set_yticks([int('%d000000000'%i) for i in range(10)])
+ax2.set_yticklabels(range(10))
+ax1.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax1.set_xticklabels('')
+ax2.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1,7]])
+ax2.set_xticklabels([datetime.date(i,j,1).strftime('%b %Y')  for i in range(2013,2019) for j in [1,7]])
+ax1.plot(eth_market_info['Date'].astype(datetime.datetime),eth_market_info['eth_Open'])
+ax2.bar(eth_market_info['Date'].astype(datetime.datetime).values, eth_market_info['eth_Volume'].values)
+fig.tight_layout()
+fig.figimage(eth_im, 300, 180, zorder=3, alpha=.6)
+plt.show()
+
+
+market_info = pd.merge(bitcoin_market_info,eth_market_info, on=['Date'])
+market_info = market_info[market_info['Date']>='2016-01-01']
+for coins in ['bt_', 'eth_']: 
+    kwargs = { coins+'day_diff': lambda x: (x[coins+'Close']-x[coins+'Open*'])/x[coins+'Open*']}
+    market_info = market_info.assign(**kwargs)
+market_info.head()
