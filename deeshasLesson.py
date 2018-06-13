a04ace471463883b45b8523f00ba9302c3c7ad60
Lord Afzal <afzal.eliyah@gmail.com>
2018-06-13 02:05:07 -0400

    eth

--- deeshasLesson.py
+++ deeshasLesson.py
@@ -22,3 +22,9 @@ bitcoin_market_info.loc[bitcoin_market_info['Volume']=="-",'Volume']=0
 bitcoin_market_info['Volume'] = bitcoin_market_info['Volume'].astype('int64')
 # look at the first few rows
 bitcoin_market_info.head()
+# get market info for ethereum from the start of 2016 to the current day
+eth_market_info = pd.read_html("https://coinmarketcap.com/currencies/ethereum/historical-data/?start=20130428&end="+time.strftime("%Y%m%d"))[0]
+# convert the date string to the correct date format
+eth_market_info = eth_market_info.assign(Date=pd.to_datetime(eth_market_info['Date']))
+# look at the first few rows
+eth_market_info.head()
